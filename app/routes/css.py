c531a8fb29c7142e2e1ca4923fabdd76edee3e2a
evollivia <ytafxt@naver.com>
2024-07-31 12:19:40 +0900

    css3 - 선택자, css리셋

--- app/routes/css.py
+++ app/routes/css.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
 # 라우터생성
@@ -6,4 +6,48 @@ css_router = APIRouter()
 # 템플릿 지정
 templates = Jinja2Templates(directory='views/templates')
 
+
 # 라우터 설정
+@css_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('css/index.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/01selector.html', {'request': req})
+
+
+@css_router.get('/reset')
+async def reset(req: Request):
+    return templates.TemplateResponse('css/02reset.html', {'request': req})
+
+
+@css_router.get('/text')
+async def text(req: Request):
+    return templates.TemplateResponse('css/03text.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/04selector.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/05selector.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/06selector.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/07selector.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/08selector.html', {'request': req})
