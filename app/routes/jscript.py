823fcb7f6a20544c78bd93260583c3cd6e19ec5d
evollivia <ytafxt@naver.com>
2024-08-06 09:27:48 +0900

    자바스크립트 개요, 자료형

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
 # 라우터생성
@@ -6,4 +6,18 @@ jscript_router = APIRouter()
 # 템플릿 지정
 templates = Jinja2Templates(directory='views/templates')
 
+
 # 라우터 설정
+@jscript_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('js/index.html', {'request': req})
+
+
+@jscript_router.get('/hello')
+async def hello(req: Request):
+    return templates.TemplateResponse('js/01hello.html', {'request': req})
+
+
+@jscript_router.get('/type')
+async def type(req: Request):
+    return templates.TemplateResponse('js/02type.html', {'request': req})
